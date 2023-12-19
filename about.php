14e87631538426d977ae964a6cc53c211c18e573
22091397033 Aryangga Putra Satrya <117990980+22091397033AryanggaPutraSatrya@users.noreply.github.com>
2023-12-19 18:35:43 +0700

    Update about.php

--- about.php
+++ about.php
@@ -27,25 +27,30 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 ?>
 <!DOCTYPE html>
-<html>
-<head>
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <title>LibraWeb</title>
-</head>
-<body>
-    <header>
-        <nav>
-            <ul>
-                <li><a href="index.html">Beranda</a></li>
-                <li><a href="daftar.html">Daftar Buku</a></li>
-                <li><a href="about.php">About Us</a></li>
-            </ul>
-        </nav>
-        <h1>LibraWeb</h1>
-    </header>
-<body>
+<html lang="en">
 
-<div class="main-content">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LibraWeb</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito+Sans:opsz,wght@6..12,700&display=swap">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    <link rel="stylesheet" href="about.css">
+</head>
+
+<body>
+    <!-- Bagian Header -->
+    <header>
+        <a href="" class="logo">LibraWeb</a>   
+        <div class="icons">
+            <a href="beranda.html" class="fas fa-home"></a>
+            <a href="about.php" class="fas fa-bars"></a>
+            <a href="book.html" class="fas fa-book"></a>
+        </div>
+    </header>
+    <!-- Akhir Bagian Header -->
+
+    <div class="main-content">
         <div class="about-content">
             <h2>Tentang Kami</h2>
             <p>Selamat datang di LibraWeb. Kami adalah sumber bacaan online yang berdedikasi untuk menyediakan akses ke berbagai jenis buku dan pengetahuan. Tim kami berkomitmen untuk memberikan pengalaman membaca yang berkualitas.</p>
@@ -62,10 +67,42 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 <button type="submit">Kirim Pesan</button>
             </form>
         </div>
-</div>
+    </div>
 
-    <footer>
-        &copy; 2023 LibraWeb
-    </footer>
+    <!-- Bagian Footer -->
+    <section class="footer">
+        <div class="footer-container">
+            <div class="box">
+                <h3>Dapatkan Bantuan</h3>
+                <a href="#">FAQ</a>
+                <a href="#">Kebijakan Privasi</a>
+            </div>
+
+            <div class="box">
+                <h3>Tautan Cepat</h3>
+                <a href="beranda.html">Beranda</a>
+                <a href="book.html">Daftar Buku</a>
+                <a href="about.html">Bantuan</a>
+            </div>
+
+            <div class="box" id="contact">
+                <h3>Info Kontak</h3>
+                <a href="#">+62-812-1723-4228</a>
+                <a href="" class="lower">libraweb@gmail.com</a>
+                <a href="#">Surabaya, Indonesia - 60230</a>
+            </div>
+
+            <div class="social-media" id="follow">
+                <h3>Ikuti Kami</h3>
+                <a href="https://web.facebook.com/" target="_blank"><img src="assets/fb.png"
+                        alt="Facebook"></a>
+                <a href="https://www.instagram.com/" target="_blank"><img src="assets/ig.png"
+                        alt="Instagram"></a>
+                <a href="https://tiktok.com/" target="_blank"><img src="assets/tiktok.png" alt="Tiktok"></a>
+            </div>
+        </div>
+        <div class="copyright">Seluruh Hak Cipta &copy; hak cipta 2023 oleh <span>LibraWeb</span></div>
+    </section>
+    <!-- Akhir Bagian Footer -->
 </body>
 </html>
