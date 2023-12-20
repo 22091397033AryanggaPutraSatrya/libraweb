624fb4297bcfb979a37d48defeea8066370064c4
22091397033 Aryangga Putra Satrya <117990980+22091397033AryanggaPutraSatrya@users.noreply.github.com>
2023-12-20 09:32:56 +0700

    Update about.php

--- about.php
+++ about.php
@@ -53,54 +53,30 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <div class="main-content">
         <div class="about-content">
             <h2>Tentang Kami</h2>
-            <p>Selamat datang di LibraWeb. Kami adalah sumber bacaan online yang berdedikasi untuk menyediakan akses ke berbagai jenis buku dan pengetahuan. Tim kami berkomitmen untuk memberikan pengalaman membaca yang berkualitas.</p>
+            <p>Selamat datang di LibraWeb! Kami adalah teman setia Anda dalam petualangan membaca online. Kami berkomitmen untuk membuat dunia buku dan pengetahuan lebih mudah diakses oleh semua orang. Di LibraWeb, kami ingin membuat pengalaman membaca Anda menyenangkan dan tak terlupakan dengan menyajikan beragam konten berkualitas.</p>
+            <P>Anda bisa menghubungi kami melalui:</p>    
+            <p>Telepon/WhatsApp: +62 812-1723-4228
+            <p>Instagram: @libraweb
+            <p>Email: libraweb@gmail.com
+            <p>Alamat: Surabaya, Indonesia - 60230</P>
         </div>
 
         <div class="contact-content">
             <h2>Hubungi Kami</h2>
             <p>Jika Anda memiliki pertanyaan, masukan, atau permintaan, jangan ragu untuk menghubungi kami. Isi formulir di bawah ini, dan kami akan merespons secepat mungkin.</p>
             
-            <form class="contact-form" method="post">
+
+            <form class="contact-form" action="proses.php" method="post">
                 <input type="text" name="nama" placeholder="Nama" required>
                 <input type="email" name="email" placeholder="Email" required>
                 <textarea name="pesan" placeholder="Pesan" required></textarea>
-                <button type="submit">Kirim Pesan</button>
+                <button type="submit">send</button>
             </form>
         </div>
     </div>
 
     <!-- Bagian Footer -->
     <section class="footer">
-        <div class="footer-container">
-            <div class="box">
-                <h3>Dapatkan Bantuan</h3>
-                <a href="">FAQ</a>
-                <a href="">Kebijakan Privasi</a>
-            </div>
-
-            <div class="box">
-                <h3>Tautan Cepat</h3>
-                <a href="beranda.html">Beranda</a>
-                <a href="book.html">Daftar Buku</a>
-                <a href="about.php">Tentang Kami</a>
-            </div>
-
-            <div class="box" id="contact">
-                <h3>Info Kontak</h3>
-                <a href="">+62-812-1723-4228</a>
-                <a href="" class="lower">libraweb@gmail.com</a>
-                <a href="">Surabaya, Indonesia - 60230</a>
-            </div>
-
-            <div class="social-media" id="follow">
-                <h3>Ikuti Kami</h3>
-                <a href="https://web.facebook.com/" target="_blank"><img src="assets/fb.png"
-                        alt="Facebook"></a>
-                <a href="https://www.instagram.com/" target="_blank"><img src="assets/ig.png"
-                        alt="Instagram"></a>
-                <a href="https://tiktok.com/" target="_blank"><img src="assets/tiktok.png" alt="Tiktok"></a>
-            </div>
-        </div>
         <div class="copyright">Seluruh Hak Cipta &copy; hak cipta 2023 oleh <span>LibraWeb</span></div>
     </section>
     <!-- Akhir Bagian Footer -->
